9723daf8744e60193b25ee66641b78bd3a4d7106
artem-mutaf <123882882+artem-mutaf@users.noreply.github.com>
2024-04-12 14:30:06 +0300

    Add files via upload

---
+++ tema19prkt2/ClassRoom5/TVSet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom5
+{
+    class TVSet:ISwitchhable
+    {
+        public void On()
+        {
+
+        }
+        public void Off()
+        {
+
+        }
+    }
+}

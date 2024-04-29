eb9a4fdcb7b3f75fb861ce9748a77c2da89f07c3
artem-mutaf <123882882+artem-mutaf@users.noreply.github.com>
2024-04-29 09:47:49 +0300

    Add files via upload

---
+++ PerechenPrkt/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerechenPrkt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Введите длину 1 стороны: ");
+            rectangle.Side1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите длину 2 стороны: ");
+            rectangle.Side2 = double.Parse(Console.ReadLine());
+
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            
+            Console.WriteLine("\tИнфо:\n");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\tПериметр прямоугольника = "+"<"+rectangle.PerimeterCalculator()+">");
+            Console.WriteLine("\tПлощадь прямоугольника = " + "<"+rectangle.AreaCalculator()+">"+"\n");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Нажмите любую клавишу для закрытия консоли...");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PerechenPrkt/Rectangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerechenPrkt
+{
+    class Rectangle
+    {
+        double side1;
+        double side2;
+        public double Side1 { get; set; }
+        public double Side2 { get; set; }
+        public Rectangle() { }
+
+        public Rectangle(double side1,double side2)
+        {
+            Side1 = side1;
+            Side2 = side2;
+        }
+
+        public double PerimeterCalculator()
+        {
+            return (Side1 + Side2) *2;
+        }
+
+        public double AreaCalculator()
+        {
+            return Side1 *Side2;
+        }
+
+        double Area { get; }
+        double Perimeter { get; }
+
+        //.
+    }
+}

014e9a2ac07bf18f21a2c102b77cd0bd727e0fbc
artem-mutaf <123882882+artem-mutaf@users.noreply.github.com>
2024-05-20 09:48:00 +0300

    Add files via upload

---
+++ Повторение2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Повторение2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Введите сторону а1 ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Введите сторону b1 ");
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Введите сторону c1 ");
+            //int c = Convert.ToInt32(Console.ReadLine());
+            Triangle<int> triangle1 = new Triangle<int>(1,2,3);
+            triangle1.Print();
+            Console.WriteLine($"Площадь = {triangle1.Area()}");
+            Triangle<double> triangle2 = new Triangle<double>(2.5,3.3,4.2);
+            triangle2.Print();
+            Console.WriteLine($"Площадь = {triangle2.Area()}");
+            Triangle<uint> triangle3 = new Triangle<uint>(2u, 3u, 4u);
+            triangle3.Print();
+            Console.WriteLine($"Площадь = {triangle3.Area()}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Повторение2/Triangle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Повторение2
+{
+    class Triangle<T>
+    {
+        private T a;
+        private T b;
+        private T c;
+
+        public T A
+        {
+            set { value = a; }
+            get { return a; }
+        }
+        public T B
+        {
+            set { value = b; }
+            get { return b; }
+        }
+        public T C
+        {
+            set { value = c; }
+            get { return c; }
+        }
+        public Triangle() { }
+        public Triangle(T a,T b, T c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"A = {A}\nB = {B}\nC = {C}\n");
+        }
+        public double Area()
+        {
+            dynamic a1 = A;
+            dynamic b1 = B;
+            dynamic c1 = C;
+            double p = (double)(a1 + b1 + c1) / 2;
+            return Math.Sqrt((dynamic)p * ((dynamic)p - a1) * ((dynamic)p - b1) * ((dynamic)p - c1));
+        }
+
+
+    }
+}

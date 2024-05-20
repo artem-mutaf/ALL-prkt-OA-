1d40096a74935056024683241e6fe9316de89f71
artem-mutaf <123882882+artem-mutaf@users.noreply.github.com>
2024-05-20 09:47:30 +0300

    Add files via upload

---
+++ Повторение1/Point.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Повторение1
+{
+    class Point <T>
+    {
+        //Внутренние поля
+        T x;
+        T y;
+        //Свойства
+        public T X { get; }
+        public T Y { get; }
+        //Конструкторы
+        public Point() { }
+        public Point(T x,T y) 
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void Info()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"Значение x = {X} , Значение y = {Y}");
+        }
+
+    }
+}
---
+++ Повторение1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Повторение1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point<int> point1 = new Point<int>(5, 3);
+            Point<double> point2 = new Point<double>(5.3, 4.6);
+            Point<uint> point3 = new Point<uint>(34u, 54u);
+
+            point1.Info();
+            point2.Info();
+            point3.Info();
+
+            Console.ReadKey();
+
+        }
+    }
+}

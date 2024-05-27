31748d5fd7fa34fc5fc8ccc71a42e3cc9bc2d4d7
artem-mutaf <123882882+artem-mutaf@users.noreply.github.com>
2024-05-27 09:50:50 +0300

    Add files via upload

---
+++ Задача1Перечисления/Oper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача1Перечисления
+{
+    class Oper
+    {
+        //public Oper() { }
+        public static double DoOperation(double x, double y, Operation op)
+        {
+            double result = 0;
+            //{
+            //    Operation.Add => x + y,
+            //    Operation.Subtract => x - y,
+            //    Operation.Multiply => x * y,
+            //    Operation.Divide => x / y
+            //};
+            switch(op)
+            {
+                case Operation.Add:
+                    result = x + y;
+                    break;
+                case Operation.Subtract:
+                    result = x - y;
+                    break;
+                case Operation.Multiply:
+                    result = x * y;
+                    break;
+                case Operation.Divide:
+                    result = x / y;
+                    break;
+                default:
+                    Console.WriteLine("Вы выбрали не правильный вариант");
+                    break;
+            }
+            return result;
+        }
+        public enum Operation
+        {
+            Add,
+            Subtract,
+            Multiply,
+            Divide
+        }
+    }
+}
---
+++ Задача1Перечисления/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача1Перечисления
+{
+    //enum Month
+    //{
+    //    Январь,Февраль,Март,Апрель,Май,Июнь,Июль,Август,Сентябрь,Октябрь,Ноябрь,Декабрь
+    //}
+    //enum Days
+    //{
+    //    Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье
+    //}
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Month i;
+            //for (i = Month.Январь; i <= Month.Декабрь; i++)
+            //    Console.WriteLine($"{(int)i} \t  {i}");
+
+            //Days i;
+            //for (i = Days.Понедельник; i <= Days.Воскресенье; i++)
+            //    Console.WriteLine($"{i} имеет значение -  {(int)i}");
+            //Oper operation = new Oper();
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+            Console.WriteLine(Oper.DoOperation(x, y,Oper.Operation.Add));
+            Oper.DoOperation(x, y, Oper.Operation.Subtract);
+            Oper.DoOperation(x, y, Oper.Operation.Multiply);
+            Oper.DoOperation(x, y, Oper.Operation.Divide);
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
